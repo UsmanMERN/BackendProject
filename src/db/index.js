8f1fffaa4514309a278487efbe877f17ebdb2ba6
UsmanMern <mu1363131@gmail.com>
2024-01-21 10:18:15 +0500

    utils code

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`).then(
-            console.log(`\n database is connected`)
-        )
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
         console.log(`\n database is connected ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MongoDB connection error", error);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import { config } from "dotenv";
+import { config } from "dotenv ";
 import express from "express";
 import connectDB from "./db/index.js";
 
 // Load environment variables from the .env file
 config();
 
-
 // Create an instance of the express application
 const app = express();
 
-connectDB()
-// Start the express app after successful database connection
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`App is listening on port ${process.env.PORT || 8080}`);
-});
+
+
+// PORT
+const PORT = process.env.PORT || 8080
+// dataBase is connected
+connectDB().then(() => {
+    // Start the express app after successful database connection
+    app.listen(PORT, () => {
+        console.log(`App is listening on port ${PORT}`);
+    })
+}
+)
